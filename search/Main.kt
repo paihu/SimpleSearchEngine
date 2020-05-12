4ebed213db31d560a3ace9e65e42f5dad2c87dd6
paihu <paihu_j@yahoo.co.jp>
2020-05-12 23:22:50 +0900

    Stage #2

--- search/Main.kt
+++ search/Main.kt
@@ -1,15 +1,25 @@
 package search
 
 fun main() {
-    val input = readLine()!!.split(" ")
-    val searchWord = readLine()
-
-    for ( i in 1..input.size){
-        if (searchWord == input[i-1]) {
-            print(i)
-            return
+    println("Enter the number of people:")
+    val numPeoples = readLine()!!.toInt()
+    val peoples: MutableList<String> = mutableListOf()
+    for ( i in 1..numPeoples){
+        peoples.add(readLine()!!)
+    }
+    println("Enter the number of search queries:")
+    val numQueries = readLine()!!.toInt()
+    for ( i in 1..numQueries){
+        val searchWord = readLine()!!.toUpperCase()
+        if (peoples.find{ people -> people.toUpperCase().contains(searchWord)} is String){
+            println("Found people: ")
+            for (people in peoples){
+                if (people.toUpperCase().contains(searchWord)){
+                    println(people)
+                }
+            }
+        } else{
+            println("No matching people found.")
         }
     }
-    print("Not found")
-
 }
