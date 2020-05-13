e486eb7d590c1e1a2a2150748654313b49bf589f
paihu <paihu_j@yahoo.co.jp>
2020-05-13 20:33:37 +0900

    Stage #3

--- search/Main.kt
+++ search/Main.kt
@@ -1,25 +1,37 @@
 package search
 
+class SearchEngine(val peoples: List<String>) {
+    val menu = listOf("Find a person", "Print all people", "Exit")
+    fun printMenu() {
+        println("=== Menu ===")
+        println("1. Find a person")
+        println("2. Print all people")
+        println("0. Exit")
+    }
+
+    fun findAll():List<String> {
+        return peoples
+    }
+    fun findPeople(searchWord: String):List<String> {
+        return peoples.filter { v-> v.toUpperCase().contains(searchWord.toUpperCase())}
+    }
+}
+
 fun main() {
-    println("Enter the number of people:")
+    println("Enter all people:")
     val numPeoples = readLine()!!.toInt()
     val peoples: MutableList<String> = mutableListOf()
     for ( i in 1..numPeoples){
         peoples.add(readLine()!!)
     }
-    println("Enter the number of search queries:")
-    val numQueries = readLine()!!.toInt()
-    for ( i in 1..numQueries){
-        val searchWord = readLine()!!.toUpperCase()
-        if (peoples.find{ people -> people.toUpperCase().contains(searchWord)} is String){
-            println("Found people: ")
-            for (people in peoples){
-                if (people.toUpperCase().contains(searchWord)){
-                    println(people)
-                }
-            }
-        } else{
-            println("No matching people found.")
-        }
-    }
+    val searchEngine = SearchEngine(peoples)
+    loop@ do {
+       searchEngine.printMenu()
+       when(readLine()!!){
+           "1" -> {println("Enter a name or email to search all suitable people."); searchEngine.findPeople(readLine()!!).forEach { v->println(v)}}
+           "2" -> {println("=== List of people ===");searchEngine.findAll().forEach{v->println(v)}}
+           "0"-> break@loop
+       }
+    }while(true)
+    println("Bye!")
 }
