5a565b79ac9e2a2cf83c8207b39970b41eb4212e
paihu <paihu_j@yahoo.co.jp>
2020-05-14 22:41:18 +0900

    refactoring

--- search/Main.kt
+++ search/Main.kt
@@ -1,48 +1,58 @@
 package search
 import java.io.File
 
-class SearchEngine(private val peoples: List<String>) {
+class SearchEngine(private val database: List<String>) {
     private val searchIndex: Map<String,List<Int>> =makeSearchIndex()
 
     private fun makeSearchIndex(): Map<String,List<Int>> {
         val searchIndex: MutableMap<String,List<Int>>  = mutableMapOf()
-        peoples.forEachIndexed(){ i, people -> people.split(" ").forEach {
+        database.forEachIndexed(){ i, people -> people.split(" ").forEach {
             key -> searchIndex[key.toUpperCase()] = searchIndex.getOrDefault(key.toUpperCase(), mutableListOf())+i
         }}
         return searchIndex
     }
-
-    fun getMenu(): List<String>{
-        return listOf(
-                "1. Find a person",
-                "2. Print all people",
-                "0. Exit"
-        )
-    }
-
     fun findAll(): List<String>{
-        return peoples
+        return database
     }
-    fun findPeopleNumber(searchWord: String): Int {
-        return searchIndex.getOrDefault(searchWord.toUpperCase(),listOf()).size
-    }
-    fun findPeople(searchWord: String):List<String> {
-        return searchIndex.getOrDefault(searchWord.toUpperCase(),listOf()).map{v -> peoples[v]}
-    }
-    fun findPeopleIndex(searchWord: String):List<Int> {
+    private fun findIndex(searchWord: String):List<Int> {
         return searchIndex.getOrDefault(searchWord.toUpperCase(),listOf())
     }
+    private fun findAny(searchWords: List<String>): List<Int>{
+        return searchWords.fold(listOf<Int>(), {acc, v -> acc + findIndex(v)}).distinct().sorted()
+    }
+    private fun findAnd(searchWords: List<String>): List<Int>{
+        return searchWords.fold(database.indices.toList(), { acc, v -> acc.filter{ t-> findIndex(v).contains(t) }})
+    }
+    private fun findNone(searchWords: List<String>): List<Int>{
+       return  searchWords.fold(database.indices.toList(), { acc, v -> acc.filter{ t -> !findIndex(v).contains(t)}})
+    }
+    private fun findIndex(strategy: String, searchWords: List<String>): List<Int>{
+        return when (strategy) {
+            "ANY" -> findAny(searchWords)
+            "ALL" -> findAnd(searchWords)
+            "NONE" -> findNone(searchWords)
+            else -> listOf()
+        }
+    }
+    fun find(strategy: String, searchWords: List<String>): List<String>
+    {
+        return findIndex(strategy,searchWords).map{v-> database[v]}
+    }
+}
 
-    fun findAnd(searchWords: List<String>): List<String>{
-        return searchWords.fold(this, {acc, v -> SearchEngine(acc.findPeople(v))}).findAll()
-    }
-    fun findAny(searchWords: List<String>): List<String>{
-        return searchWords.fold(listOf<Int>(), {acc, v -> acc + findPeopleIndex(v)}).toSet().toList().sorted().map{v-> peoples[v]}
-    }
-    fun findNone(searchWords: List<String>): List<String>{
-       val notReturnIndex =  searchWords.fold(listOf<Int>(), {acc, v -> acc + findPeopleIndex(v)}).toSet()
-       return peoples.indices.fold(listOf<String>() ,{ acc, v -> if (notReturnIndex.contains(v)) acc else acc + peoples[v]})
-    }
+fun getMenu(): List<String>{
+    return listOf(
+            "1. Find a person",
+            "2. Print all people",
+            "0. Exit"
+    )
+}
+fun getStrategy(): List<String>{
+    return listOf(
+            "ALL",
+            "ANY",
+            "NONE"
+    )
 }
 fun main(args: Array<String>) {
     val fileName = args[1]
@@ -50,18 +60,14 @@ fun main(args: Array<String>) {
     val searchEngine = SearchEngine(peoples)
     loop@ do {
        println("=== Menu ===")
-       searchEngine.getMenu().forEach { v -> println(v)}
+       getMenu().forEach { v -> println(v)}
        when(readLine()!!){
            "1" -> {
-               println("Select a matching stragegy: ALL, ANY, NONE")
+               println("Select a matching strategy: %s".format(getStrategy().joinToString(", ")))
                val strategy = readLine()!!
                println("Enter a name or email to search all suitable people.")
-               val searchWords = readLine()!!.split(" ").filter{ v-> v.length>0}
-               val finds = when (strategy){
-                   "ALL" -> searchEngine.findAnd(searchWords)
-                   "ANY" -> searchEngine.findAny(searchWords)
-                   else -> searchEngine.findNone(searchWords)
-               }
+               val searchWords = readLine()!!.split(" ").filter{ v-> v.isNotEmpty() }
+               val finds = searchEngine.find(strategy, searchWords)
                when (finds.size){
                    0 -> println("No matching people found.")
                    else -> {
@@ -69,7 +75,6 @@ fun main(args: Array<String>) {
                        finds.forEach { v->println(v)}
                    }
                }
-
            }
            "2" -> {println("=== List of people ===");searchEngine.findAll().forEach{v->println(v)}}
            "0"-> break@loop
