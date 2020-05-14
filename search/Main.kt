1ae32c4b0d912b4553842565f713d85231ad9fb6
paihu <paihu_j@yahoo.co.jp>
2020-05-14 21:23:59 +0900

    Stage #6

--- search/Main.kt
+++ search/Main.kt
@@ -29,6 +29,20 @@ class SearchEngine(private val peoples: List<String>) {
     fun findPeople(searchWord: String):List<String> {
         return searchIndex.getOrDefault(searchWord.toUpperCase(),listOf()).map{v -> peoples[v]}
     }
+    fun findPeopleIndex(searchWord: String):List<Int> {
+        return searchIndex.getOrDefault(searchWord.toUpperCase(),listOf())
+    }
+
+    fun findAnd(searchWords: List<String>): List<String>{
+        return searchWords.fold(this, {acc, v -> SearchEngine(acc.findPeople(v))}).findAll()
+    }
+    fun findAny(searchWords: List<String>): List<String>{
+        return searchWords.fold(listOf<Int>(), {acc, v -> acc + findPeopleIndex(v)}).toSet().toList().sorted().map{v-> peoples[v]}
+    }
+    fun findNone(searchWords: List<String>): List<String>{
+       val notReturnIndex =  searchWords.fold(listOf<Int>(), {acc, v -> acc + findPeopleIndex(v)}).toSet()
+       return peoples.indices.fold(listOf<String>() ,{ acc, v -> if (notReturnIndex.contains(v)) acc else acc + peoples[v]})
+    }
 }
 fun main(args: Array<String>) {
     val fileName = args[1]
@@ -39,15 +53,23 @@ fun main(args: Array<String>) {
        searchEngine.getMenu().forEach { v -> println(v)}
        when(readLine()!!){
            "1" -> {
+               println("Select a matching stragegy: ALL, ANY, NONE")
+               val strategy = readLine()!!
                println("Enter a name or email to search all suitable people.")
-               val searchWord = readLine()!!
-               when (val found: Int = searchEngine.findPeopleNumber(searchWord)){
+               val searchWords = readLine()!!.split(" ").filter{ v-> v.length>0}
+               val finds = when (strategy){
+                   "ALL" -> searchEngine.findAnd(searchWords)
+                   "ANY" -> searchEngine.findAny(searchWords)
+                   else -> searchEngine.findNone(searchWords)
+               }
+               when (finds.size){
                    0 -> println("No matching people found.")
                    else -> {
-                   println("%d persons found:".format(found))
-                   searchEngine.findPeople(searchWord).forEach { v->println(v)}
+                       println("%d persons found:".format(finds.size))
+                       finds.forEach { v->println(v)}
                    }
                }
+
            }
            "2" -> {println("=== List of people ===");searchEngine.findAll().forEach{v->println(v)}}
            "0"-> break@loop
