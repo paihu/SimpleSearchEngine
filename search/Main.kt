9393806a8c0902d5f6a26d05ba198c1736a0e08f
paihu <paihu_j@yahoo.co.jp>
2020-05-14 21:23:25 +0900

    Stage #5

--- search/Main.kt
+++ search/Main.kt
@@ -1,20 +1,33 @@
 package search
 import java.io.File
 
-class SearchEngine(val peoples: List<String>) {
-    val menu = listOf("Find a person", "Print all people", "Exit")
-    fun printMenu() {
-        println("=== Menu ===")
-        println("1. Find a person")
-        println("2. Print all people")
-        println("0. Exit")
+class SearchEngine(private val peoples: List<String>) {
+    private val searchIndex: Map<String,List<Int>> =makeSearchIndex()
+
+    private fun makeSearchIndex(): Map<String,List<Int>> {
+        val searchIndex: MutableMap<String,List<Int>>  = mutableMapOf()
+        peoples.forEachIndexed(){ i, people -> people.split(" ").forEach {
+            key -> searchIndex[key.toUpperCase()] = searchIndex.getOrDefault(key.toUpperCase(), mutableListOf())+i
+        }}
+        return searchIndex
     }
 
-    fun findAll():List<String> {
+    fun getMenu(): List<String>{
+        return listOf(
+                "1. Find a person",
+                "2. Print all people",
+                "0. Exit"
+        )
+    }
+
+    fun findAll(): List<String>{
         return peoples
     }
+    fun findPeopleNumber(searchWord: String): Int {
+        return searchIndex.getOrDefault(searchWord.toUpperCase(),listOf()).size
+    }
     fun findPeople(searchWord: String):List<String> {
-        return peoples.filter { v-> v.toUpperCase().contains(searchWord.toUpperCase())}
+        return searchIndex.getOrDefault(searchWord.toUpperCase(),listOf()).map{v -> peoples[v]}
     }
 }
 fun main(args: Array<String>) {
@@ -22,9 +35,20 @@ fun main(args: Array<String>) {
     val peoples = File(fileName).readLines()
     val searchEngine = SearchEngine(peoples)
     loop@ do {
-       searchEngine.printMenu()
+       println("=== Menu ===")
+       searchEngine.getMenu().forEach { v -> println(v)}
        when(readLine()!!){
-           "1" -> {println("Enter a name or email to search all suitable people."); searchEngine.findPeople(readLine()!!).forEach { v->println(v)}}
+           "1" -> {
+               println("Enter a name or email to search all suitable people.")
+               val searchWord = readLine()!!
+               when (val found: Int = searchEngine.findPeopleNumber(searchWord)){
+                   0 -> println("No matching people found.")
+                   else -> {
+                   println("%d persons found:".format(found))
+                   searchEngine.findPeople(searchWord).forEach { v->println(v)}
+                   }
+               }
+           }
            "2" -> {println("=== List of people ===");searchEngine.findAll().forEach{v->println(v)}}
            "0"-> break@loop
        }
